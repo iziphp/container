8af37c10fa84c957bd7871a4df5295b09c084ffb
Vusal Orujov <or.vusal@gmail.com>
2024-11-15 16:14:53 +0400

    Update Inject attribute to be repeatable

--- src/Attributes/Inject.php
+++ src/Attributes/Inject.php
@@ -5,7 +5,7 @@ namespace Easy\Container\Attributes;
 use Attribute;
 
 /** @package Easy\Container\Attributes */
-#[Attribute(Attribute::TARGET_PARAMETER)]
+#[Attribute(Attribute::TARGET_PARAMETER | Attribute::IS_REPEATABLE)]
 class Inject
 {
     public function __construct(public mixed $abstract)
